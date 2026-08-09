2af0722b802a8a896807aba48fff6ec4511daf15
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2026-08-09 11:42:15 -0500

    feat: add new get mapping

--- src/main/java/cursoSpringBoot/controllers/CustomerController.java
+++ src/main/java/cursoSpringBoot/controllers/CustomerController.java
@@ -3,6 +3,7 @@ package cursoSpringBoot.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,14 +13,23 @@ import cursoSpringBoot.domain.Customer;
 
 @RestController
 public class CustomerController {
-
     private List<Customer> customers = new ArrayList<>(Arrays.asList(
-            new Customer(1, "John Doe", "johndoe", "password123"),
-            new Customer(2, "Jane Smith", "janesmith", "password456"),
-            new Customer(3, "Bob Johnson", "bobjohnson", "password789")));
+            new Customer(1, "John", "johndoe", "password123"),
+            new Customer(2, "Jane", "janesmith", "password456"),
+            new Customer(3, "Bob", "bobjohnson", "password789")));
 
     @GetMapping("/customers")
     public List<Customer> getAllCustomers() {
         return customers;
+    }
+
+    @GetMapping("/customers/{username}")
+    public Customer GetCliente(@PathVariable String username) {
+        for (Customer customer : customers) {
+            if (customer.getUsername().equalsIgnoreCase(username)) {
+                return customer;
+            }
+        }
+        return null;
     }
 }
